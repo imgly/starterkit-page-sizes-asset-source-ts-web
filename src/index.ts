c1acb4cad9a2c4386292ffed24848e9373ce84d5
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-04-28 15:42:56 +0200

    Update starterkit for v1.74.0-rc.1

--- src/index.ts
+++ src/index.ts
@@ -42,9 +42,7 @@ CreativeEditorSDK.create('#cesdk_container', config)
     // ============================================================================
 
     // Load the page sizes scene with pre-designed content
-    await cesdk.loadFromURL(
-      resolveAssetPath('/assets/page-sizes.scene')
-    );
+    await cesdk.loadFromURL(resolveAssetPath('/assets/page-sizes.scene'));
   })
   .catch((error) => {
     // eslint-disable-next-line no-console
