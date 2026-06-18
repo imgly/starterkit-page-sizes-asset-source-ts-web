470f98092976e2cfe62a6366d15ac3984735f498
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-06-18 19:51:36 +0200

    Update starterkit for v1.77.0-rc.3

--- src/imgly/config/ui/dock.ts
+++ src/imgly/config/ui/dock.ts
@@ -83,6 +83,7 @@ export function setupDock(cesdk: CreativeEditorSDK): void {
       label: 'component.library.elements',
       entries: [
         'ly.img.image',
+        'ly.img.text.presets',
         'ly.img.text',
         'ly.img.vector.shape',
         'ly.img.sticker'
@@ -115,7 +116,7 @@ export function setupDock(cesdk: CreativeEditorSDK): void {
       key: 'ly.img.text',
       icon: '@imgly/Text',
       label: 'libraries.ly.img.text.label',
-      entries: ['ly.img.text']
+      entries: ['ly.img.text.presets', 'ly.img.text']
     },
     {
       id: 'ly.img.assetLibrary.dock',
